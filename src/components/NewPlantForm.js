d84008b2b971c019fc205429ca9bb33d24342deb
Christopher <chrisjwkim97@gmail.com>
2023-12-01 10:05:02 -0500

    core

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-function NewPlantForm() {
+function NewPlantForm({addNewPlant}) {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newPlant = {
+      name : e.target.name.value,
+      image : e.target.image.value,
+      price : e.target.price.value
+    }
+    addNewPlant(newPlant);
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="name" placeholder="Plant name" />
         <input type="text" name="image" placeholder="Image URL" />
         <input type="number" name="price" step="0.01" placeholder="Price" />
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(r => r.json())
+      .then((data) => setPlants(data))
+  }, [])
+
+  function addNewPlant(newPlantObject) {
+    const newPlants = [...plants, newPlantObject];
+    setPlants(newPlants);
+
+  }
+
+  const filteredPlants = plants.filter((plant) => plant.name.toLowerCase().includes(search.toLowerCase()));
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant}/>
+      <Search search={search} setSearch={setSearch}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
